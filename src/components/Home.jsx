aeb91b6ce4b915170e47ea5bf575a538f43ac92a
Margaretha Naibaho <margarethanaibaho2@gmail.com>
2024-12-20 20:54:37 +0700

    navbar already with setActive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
         <>
             <div className="bg-[url('src/assets/images/color.jpg')] bg-cover bg-center w-full h-[400px]">
                 <div className="mx-72 py-10 flex  justify-center">
-                    <h1 className="text-white text-shadow-lg font-medium text-center w-full mb-10" 
+                    <h1 className="text-white text-shadow-lg font-medium text-center w-full" 
                         style={{ fontSize: "clamp(2rem, 15vw, 3.5rem)" }}
                     >
                         FIND THE RIGHT CARTRIDGES FOR YOUR PRINTER
@@ -12,12 +12,16 @@ const Home = () => {
 
                 <div className="mx-72">
                     <div className="flex flex-row items-center py-5">
-                        <div className="w-full flex">
-                            <div className="py-5 px-auto text-white font-bold bg-blue-500">
-                                3-stepEasySearch
+                        <div className="w-full flex flex-row justify-around">
+                            <div className="w-full py-5 px-auto text-white text-xl font-semibold bg-blue-500">
+                                <button>
+                                    <a href="#">3-stepEasySearch&reg;</a>
+                                </button>
                             </div>
-                            <div className="py-5 px-auto text-black font-bold bg-gray-200">
-                                3-stepEasySearch
+                            <div className="w-full py-5 px-auto text-black text-xl font-semibold bg-gray-200">
+                                <button>
+                                    <a href="#">Search by Serial Number</a>
+                                </button>
                             </div>
                         </div>
                     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const NavBar = () => {
     const menuItems = [
         "HOME",
@@ -7,9 +9,15 @@ const NavBar = () => {
         "LOGIN / REGISTER"
     ]
 
+    const [activeNavBar, setActiveNavBar] = useState("HOME");
+
     const NavBarItems = ({textNavBar}) => {
+        const isActive = activeNavBar === textNavBar;
         return (
-            <div className="py-5 px-auto text-white font-bold">
+            <div 
+                className={`w-full py-5 px-auto text-white font-bold ${isActive ? "bg-blue-600" : "bg-blue-500"} cursor-pointer`}
+                onClick={() => setActiveNavBar(textNavBar)}
+            >
                 <button>
                     <a href="#">{textNavBar}</a>
                 </button>
@@ -19,7 +27,7 @@ const NavBar = () => {
     return (
         <>
             <div className="flex flex-row items-center mx-72 mb-10">
-                <div className="w-full flex flex-row bg-blue-500 justify-around">
+                <div className="w-full flex flex-rowjustify-around">
                     {menuItems.map((item, index) => (
                         <NavBarItems key={index} textNavBar={item} />
                     ))}
